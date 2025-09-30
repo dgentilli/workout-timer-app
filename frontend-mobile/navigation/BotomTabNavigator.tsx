806862ffec6cb2e1969fe42c7b07e60494de5e94
Dave Gentilli <dgentilli@gmail.com>
2025-09-30 09:48:25 +0300

    Style navigation bars

--- frontend-mobile/navigation/BotomTabNavigator.tsx
+++ frontend-mobile/navigation/BotomTabNavigator.tsx
@@ -1,13 +1,26 @@
+import { useColorScheme } from '@/hooks/use-color-scheme';
 import HomeUI from '@/screens/Home/HomeUI';
 import SettingsUI from '@/screens/settings/SettingsUI';
 import WorkoutsUI from '@/screens/workouts/WorkoutsUI';
+import { themes } from '@/themes/main';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 const Tab = createBottomTabNavigator();
 
 const BottomTabNavigator = () => {
+  const { colorScheme } = useColorScheme();
+  const colors = themes.main.colors[colorScheme];
+
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={{
+        tabBarStyle: { backgroundColor: colors.surface },
+        tabBarActiveTintColor: colors.primary,
+        tabBarInactiveTintColor: colors.text.secondary,
+        headerStyle: { backgroundColor: colors.background },
+        headerTintColor: colors.text.primary,
+      }}
+    >
       <Tab.Screen name='Home' component={HomeUI} />
       <Tab.Screen name='Exercise' component={WorkoutsUI} />
       <Tab.Screen name='Settings' component={SettingsUI} />
